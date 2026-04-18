1b1edd15dc110c9dd50b442e01af78338c36bec6
caramelpopcornKHU <hyungmin8528@naver.com>
2026-04-18 21:56:07 +0900

    주문 - Repository, Service 구현

---
+++ src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -0,0 +1,27 @@
+package jpabook.jpashop.repository;
+
+import jakarta.persistence.EntityManager;
+import jpabook.jpashop.domain.Order;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class OrderRepository {
+
+    private final EntityManager em;
+
+    public void save(Order order) {
+        em.persist(order);
+    }
+
+    public Order fineOne(Long id) {
+        return em.find(Order.class, id);
+    }
+
+    // 유저이름으로 조회 - 동적쿼리 생성
+//    public List<Order> findAll(OrderSearch orderSearch){}
+
+}
