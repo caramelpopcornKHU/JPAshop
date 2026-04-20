e53f53ad9ee3e054638ee9b0913481b881b8df11
caramelpopcornKHU <hyungmin8528@naver.com>
2026-04-20 12:06:04 +0900

    재고수량 예외 - 리펙토링 반영 엔티티

--- src/main/java/jpabook/jpashop/domain/item/Item.java
+++ src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -2,7 +2,7 @@ package jpabook.jpashop.domain.item;
 
 import jakarta.persistence.*;
 import jpabook.jpashop.domain.Category;
-import jpabook.jpashop.exception.NoEnoughStockException;
+import jpabook.jpashop.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -44,7 +44,7 @@ public abstract class Item {
         int restStock = this.stockQuantity - quantity;
 
         if (restStock < 0) {
-            throw new NoEnoughStockException("need more stock");
+            throw new NotEnoughStockException("need more stock");
         }
         this.stockQuantity = restStock;
 
